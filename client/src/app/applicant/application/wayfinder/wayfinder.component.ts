5af62d6f82a25c9846c6f033b8eb612c2856c83e
levpolshkov <polshkov.lv@gmail.com>
2021-09-08 16:40:33 -0500

    Change Wayfinder to SVG.  Sexy clean lines.

--- client/src/app/applicant/application/wayfinder/wayfinder.component.ts
+++ client/src/app/applicant/application/wayfinder/wayfinder.component.ts
@@ -1,6 +1,30 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { ApplicationSection } from '../../../models';
-import { CanvasComponent } from '../canvas/canvas.component';
+
+
+interface Title {
+	x: number,
+	y: number,
+	color: string,
+	text: string
+}
+interface Circle {
+	x: number,
+	y: number,
+	color: string
+};
+
+interface Line {
+	x1: number,
+	x2: number,
+	y: number,
+	color: string
+};
+
+interface Section extends ApplicationSection {
+	completed?: number,
+	current?: boolean
+};
 
 @Component({
 	selector: 'wayfinder',
@@ -8,31 +32,81 @@ import { CanvasComponent } from '../canvas/canvas.component';
 	styleUrls: ['./wayfinder.component.scss']
 })
 export class WayfinderComponent implements OnInit {
-	@ViewChild('canvas') canvas:CanvasComponent;
-	@Input() sections:any[] = [];
+	@Input() sections:Section[] = [];
+	@Input() sectionIndex:number;		// Currently active sectionIndex
+
+	width = 700;
+	height = 100;
+
+	titles:Title[] = [];
+	circles:Circle[] = [];
+	lines:Line[] = [];
+
+	completedColor = '#4e40f3';
+	grayColor = '#ccc';
 
 	constructor() { }
 
-	ngOnInit() {
 
+	ngOnChanges(changes:SimpleChanges) {
+		if(changes.sectionIndex && changes.sectionIndex.currentValue) {
+			console.log('ngOnChanges: changes=%o', changes);
+			this.sections.forEach(s => s.current=false);
+			this.sections[changes.sectionIndex.currentValue].current = true;
+			this.draw();
+		}
 	}
 
-	ngAfterViewInit() {
-		console.log('AppComponent canvas: %o', this.canvas);
+	ngOnInit() {
+		this.draw();
+	}
 
-		// this.canvas.drawCircle(10,10,5,'blue','blue');
+	draw() {
+		const sectionWidth = (this.width)/this.sections.length;
 
-		this.sections = [1,2,3,4,5];
+		this.sections[0].completed = 1;
+		this.sections[1].completed = 0.3;
+		this.sections[this.sectionIndex].current = true;
 
-		const sectionWidth = (this.canvas.width)/this.sections.length;
-	
+		this.circles = [];
+		this.titles = [];
+		this.lines = [];
 		this.sections.forEach((section,i) => {
+			if(!section.completed) section.completed = 0;
 			const x = (i+0.5)*sectionWidth;
-			this.canvas.context.lineWidth = 3;
-			this.canvas.context.imageSmoothingEnabled = true;
-			this.canvas.drawCircle(x, 20, 9, '#4e40f3');
-		});
+			this.titles.push({
+				x,
+				y: 30,
+				text: section.title,
+				color: section.current ? 'black' : this.grayColor
+			});
+			this.circles.push({
+				x,
+				y: 50,
+				color: section.completed ? this.completedColor : this.grayColor
+			});
 
+			if(i) {
+				const previousCircle = this.circles[i-1];
+				const x1 = previousCircle.x;
+				const x3 = this.circles[i].x;
+				const x2 = x1 + (x3-x1)*this.sections[i-1].completed;
+
+				this.lines.push({
+					x1,
+					x2,
+					y: 50,
+					color: this.completedColor
+				});
+
+				this.lines.push({
+					x1: x2,
+					x2: x3,
+					y: 50,
+					color: this.grayColor
+				});
+			}
+		});
 	}
 
 }
